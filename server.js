b27e40c6ba7450aebf0ef35cdc335c4b4062edc2
Selena <73947858+selenabornstein@users.noreply.github.com>
2020-11-05 19:31:51 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use('body-parser');
 
 const mockUserData=[
 	{name: 'Mark'},
@@ -13,7 +15,7 @@ app.get('/users', function(req, res) {
 			users: mockUserData
 		})
 })
-	
+// colons are used as variable that be viewed in the params
 app.get('/users/:id', function(req, res){
 		console.log(req.params.id)
 		res.json({
@@ -21,6 +23,28 @@ app.get('/users/:id', function(req, res){
 			message: 'got one user',
 			user: req.params.id
 		})
-	})
-app.listen(8000, function() {
-console.log("server is listening")})
+})
+app.post('/login, function(req,res){
+	const username = req.body.username;
+	const password = req.body.password;
+	
+	//this should come from the database
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+	
+	if (username===mockUsername && password===mockPassword){
+		
+		res.json({
+			success: true, 
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	}
+	else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
+})
+app.listen(8000,function(){console.log('server is listening')})
