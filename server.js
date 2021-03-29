a95dfd67b67009d3072958f5c54801bdcf304541
Selena <73947858+selenabornstein@users.noreply.github.com>
2021-03-28 22:22:39 -0400

    initial file setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json())
 
 const mockUserData=[
@@ -47,5 +48,6 @@ app.post('/login', function(req,res){
 		})
 	}
 })
+
 app.listen(8000,function(){console.log('server is listening')
 })
